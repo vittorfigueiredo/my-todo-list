73c4b5f8a5db0344049aeef9f3917ce2c2e85c7a
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2021-08-29 20:50:14 -0300

    controller para deletar uma task

---
+++ src/controllers/delete-task.php
@@ -0,0 +1,31 @@
+<?php
+
+require __DIR__ . "/../database/custom-pdo.php";
+
+$taskId = $_REQUEST["task_id"];
+
+if ($taskId !== "") {
+  $sql = "delete from task where id = '$taskId'";
+  $statement = $pdo->query($sql);
+  $result = $statement->execute();
+
+  if ($result) {
+    echo json_encode([
+      "success" => true,
+      "message" => "Task deletada com sucesso!"
+    ]);
+    die;
+  }
+
+  echo json_encode([
+    "success" => false,
+    "message" => "Não foi possivel deletar a task!"
+  ]);
+  die;
+}
+
+echo json_encode([
+  "success" => false,
+  "message" => "Parâmetro task_id obrigatório!"
+]);
+die;
