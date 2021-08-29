99c894d094338c4ea8e4f1239eae7310b20f223c
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2021-08-29 03:17:55 -0300

    controller para listar tasks

---
+++ src/controllers/list-tasks.php
@@ -0,0 +1,22 @@
+<?php
+
+require __DIR__ . "/../database/custom-pdo.php";
+
+$sql = "select * from task";
+$statement = $pdo->prepare($sql);
+$statement->execute();
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+$quantidade_tasks = count($results);
+
+if ($quantidade_tasks > 0) {
+  echo json_encode([
+    "success" => true,
+    "tasks" => $results
+  ]); die;
+}
+
+echo json_encode([
+  "success" => true,
+  "tasks" => []
+]); die;
