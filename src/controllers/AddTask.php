8fca682a4d09508741eef4f3a66f7396b38c01b1
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2021-08-29 02:38:22 -0300

    controller para adicionar uma no task

---
+++ src/controllers/AddTask.php
@@ -0,0 +1,39 @@
+<?php
+
+require __DIR__ . "/../database/custom-pdo.php";
+require __DIR__ . "/../functions/uuid-generate.php";
+
+$body = $_REQUEST;
+
+foreach (["name", "description"] as $param) {
+  if (!array_key_exists($param, $body)) {
+    echo json_encode([
+      "success" => false,
+      "message" => "Parâmetro $param obrigatório!"
+    ]);
+    die;
+  }
+}
+
+$uuid = uuidGenerate();
+$name = $body["name"];
+$description = $body["description"];
+
+$sql = "insert into task set id = '$uuid', name = '$name', description = '$description'";
+$statement = $pdo->prepare($sql);
+$result = $statement->execute();
+
+
+if ($result) {
+  echo json_encode([
+    "success" => true,
+    "message" => "Task adicionada com sucesso"
+  ]);
+  die;
+}
+
+echo json_encode([
+  "success" => false,
+  "message" => "Não foi possivel adicionar a task"
+]);
+die;
