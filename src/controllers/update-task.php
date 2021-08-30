98749755273ed5000664bc637166fddb404e3709
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2021-08-29 21:20:11 -0300

    controller para atualazr uma task

---
+++ src/controllers/update-task.php
@@ -0,0 +1,37 @@
+<?php
+
+require __DIR__ . "/../database/custom-pdo.php";
+
+$body = $_REQUEST;
+
+foreach (["id", "name", "description"] as $param) {
+  if (!array_key_exists($param, $body)) {
+    echo json_encode([
+      "success" => false,
+      "message" => "Parâmetro $param obrigatório!"
+    ]);
+    die;
+  }
+}
+
+$id = $_REQUEST["id"];
+$name = $_REQUEST["name"];
+$description = $_REQUEST["description"];
+
+$sql = "update task set name = '$name', description = '$description', updated_at = now() where id = '$id'";
+$statement = $pdo->prepare($sql);
+$result = $statement->execute();
+
+if ($result) {
+  echo json_encode([
+    "success" => true,
+    "message" => "Task atualizada com sucesso! "
+  ]);
+  die;
+}
+
+echo json_encode([
+  "success" => false,
+  "message" => "Não foi possivel atualizara task!"
+]);
+die;
