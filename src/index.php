f0431d4c4d6d7506096bcb677dc50baaa17e8197
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2021-08-31 10:45:39 -0300

    adiciona cors

--- src/index.php
+++ src/index.php
@@ -1,3 +1,16 @@
 <?php
 
 require __DIR__ . "/database/custom-pdo.php";
+require __DIR__ . "/middlewares/cors.php";
+
+echo json_encode([
+  "success" => true,
+  "body" => [
+    "Author" => [
+      "name" => "Vitor Figueiredo",
+      "GitHub" => "https://github.com/vitorfigueiredopb",
+      "LinkedIn" => "https://www.linkedin.com/in/vitorfigueiredopb/"
+    ],
+    "Last Update" => "2021/08/31 10:41:00"
+  ]
+]);
