2533bd83cc42723cf44305c48227bff036cc7f80
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2021-09-01 21:40:58 -0300

    alteraçao no cors

--- src/middlewares/cors.php
+++ src/middlewares/cors.php
@@ -1,14 +1,7 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-  header('Access-Control-Allow-Origin: *');
-  header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
-  header('Access-Control-Allow-Headers: token, Content-Type');
-  header('Access-Control-Max-Age: 1728000');
-  header('Content-Length: 0');
-  header('Content-Type: text/plain');
-  die();
-}
-
 header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
